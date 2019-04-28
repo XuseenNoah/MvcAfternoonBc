07e9f1cf65816eaa9753cb01cbd1396ab5143f27
xuseen muse <xuseens@live.com>
2019-04-28 05:00:54 -0700

    first commit

---
+++ MvcAfternoonB/Controllers/PersonController.cs
@@ -0,0 +1,67 @@
+﻿using MvcAfternoonB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAfternoonB.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult DetailPerson()
+        {
+            Person person = new Person();
+            person.Id = 1;
+            person.Name = "Ismaciil Ahmed";
+            person.Addres = "Goljano";
+            return View("DetailPerson",person);
+        }
+        
+        public ActionResult ListPersons()
+        {
+
+            var listPerson = Person.GetListPerson();
+            return View(listPerson);
+        }
+        [OutputCache(Duration =120)]
+        public string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+
+
+        public ActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                Person.CreatePerson(person);
+                TempData["SuccesfullySaved"] = "Succesfully Saved";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MvcAfternoonB/Models/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MvcAfternoonB.Models
+{
+    public class Person
+    {
+        public static string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Db"].ToString();
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Addres { get; set; }
+        public string Phone { get; set; }
+
+        public static List<Person> GetListPerson()
+        {
+            return new List<Person> {
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+                new Person{Id=1,Name="Ahmed",Addres="Goljano",Phone="5555"},
+            };
+        }
+
+        public static void CreatePerson(Person person)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"INSERT INTO Persons VALUES (@Name,@Addres,@Phone)";
+                cmd.Parameters.AddWithValue("@Name", person.Name);
+                cmd.Parameters.AddWithValue("@Addres", person.Addres);
+                cmd.Parameters.AddWithValue("@Phone", person.Phone);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
